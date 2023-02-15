a15e8df8b18cf9612621fb865a106e824baf5844
Lukas <30857678+Lucke0051@users.noreply.github.com>
2023-02-15 12:10:11 +0100

    fix

--- bin/browser.dart
+++ bin/browser.dart
@@ -12,7 +12,6 @@ Future<void> launchUrl(String url) async {
       await Process.run("killall", ["-9", processName]);
       final ProcessResult result =
           await Process.run(browserPath, ["--ignore-certificate-errors", "--aggressive-cache-discard", "--kiosk", "--app=$url"]);
-      print(result.stdout);
       print(result.stderr);
       break;
   }
