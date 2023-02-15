823916b0fd9f820f53ee5336ec9b518d34f1473e
Lukas <30857678+Lucke0051@users.noreply.github.com>
2023-02-15 11:53:33 +0100

    fix

--- screenpanel-linux/setup.sh
+++ screenpanel-linux/setup.sh
@@ -1,3 +1,5 @@
 sudo cp screenpanel.service /etc/systemd/system/screenpanel.service
 sudo cp panel.exe /home/pi/screen.exe
 sudo cp screen.config.json /home/pi/screen.config.json
+sudo chmod 777 /home/pi/screen.exe
+sudo chmod 777 /etc/systemd/system/screenpanel.service
