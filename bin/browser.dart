3386d48da91562a5b249dc3288774fc3078570b4
Lukas <30857678+Lucke0051@users.noreply.github.com>
2023-02-15 13:39:19 +0100

    test

--- bin/browser.dart
+++ bin/browser.dart
@@ -3,20 +3,26 @@ import 'dart:io';
 import 'main.dart';
 
 Future<void> launchUrl(String url, double scale) async {
+  final List<String> browserArgs = [
+    "--ignore-certificate-errors",
+    "--aggressive-cache-discard",
+    "--device-scale-factor=$scale",
+    "--force-device-scale-factor=$scale",
+    "--kiosk",
+    "--app=$url"
+  ];
+
+  print(browserArgs);
+
   switch (os) {
     case "windows":
       await Process.run("taskkill", ["/IM", processName, "/F"]);
-      await Process.run(
-        browserPath,
-        ["--ignore-certificate-errors", "--aggressive-cache-discard", "--force-device-scale-factor=$scale", "--kiosk", "--app=$url"],
-      );
+      final ProcessResult result = await Process.run(browserPath, browserArgs);
+      print(result.stderr);
       break;
     case "linux":
       await Process.run("killall", ["-9", processName]);
-      final ProcessResult result = await Process.run(
-        browserPath,
-        ["--ignore-certificate-errors", "--aggressive-cache-discard", "--force-device-scale-factor=$scale", "--kiosk", "--app=$url"],
-      );
+      final ProcessResult result = await Process.run(browserPath, browserArgs);
       print(result.stderr);
       break;
   }
