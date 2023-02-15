1ce53670c989595d425900cd23837bbdf165ba34
Lukas <30857678+Lucke0051@users.noreply.github.com>
2023-02-15 13:18:38 +0100

    fix2

--- bin/browser.dart
+++ bin/browser.dart
@@ -8,14 +8,14 @@ Future<void> launchUrl(String url, double scale) async {
       await Process.run("taskkill", ["/IM", processName, "/F"]);
       await Process.run(
         browserPath,
-        ["--ignore-certificate-errors", "--aggressive-cache-discard", "--device-scale-factor=$scale", "--kiosk", "--app=$url"],
+        ["--ignore-certificate-errors", "--aggressive-cache-discard", "--force-device-scale-factor=$scale", "--kiosk", "--app=$url"],
       );
       break;
     case "linux":
       await Process.run("killall", ["-9", processName]);
       final ProcessResult result = await Process.run(
         browserPath,
-        ["--ignore-certificate-errors", "--aggressive-cache-discard", "--device-scale-factor=$scale", "--kiosk", "--app=$url"],
+        ["--ignore-certificate-errors", "--aggressive-cache-discard", "--force-device-scale-factor=$scale", "--kiosk", "--app=$url"],
       );
       print(result.stderr);
       break;
